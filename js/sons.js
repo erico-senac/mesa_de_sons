550b634c1e611eac617cfd4cd80ca64b06f96f46
erico-senac <professorerico@pi.senac.br>
2024-05-20 21:25:22 -0300

    Finalizada a mesa de sons

--- js/sons.js
+++ js/sons.js
@@ -1,39 +1,43 @@
-
 const documento = document.querySelector("html");
 const teclas = document.querySelectorAll('.teclas');
 const elemento_sons = document.querySelectorAll('audio');
-
 const toca_som = (nome) => {
     let idAudio = `som_${nome}`;
     const som = document.getElementById(idAudio);
     som.play();
 }
-
 teclas.forEach(tecla => {
     const getIdSom = tecla.getAttribute('id')
-
     tecla.onclick = () => {
         toca_som(getIdSom);
     }
-
     tecla.onkeydown = (evento) => {
         if (evento.code === 'Space' || evento.code === 'Enter') {
             toca_som(getIdSom);
-            tecla.classList.add('mark');
+            tecla.classList.add('ativa');
         }
     }
-
     tecla.onkeyup = () => {
-        tecla.classList.remove('mark');
+        tecla.classList.remove('ativa');
     }
 })
-
-documento.onkeydown = function (evento) {
-    elemento_sons.forEach(som => {
-        if(som.getAttribute('src') === `./sounds/${(evento.code).toLowerCase()}.wav`){
-            som.play();
-        } else {
-            console.log(`${evento.code} - ./sounds/${(evento.code).toLowerCase()}.wav - tecla errada`);
+documento.onkeydown = (evento) => {
+    const som = [...elemento_sons].filter(
+        el => (el.getAttribute('src') === `./sounds/${(evento.code).toLowerCase()}.wav`)
+    );
+    if(som.length > 0) {
+        const tecla = [...teclas].filter(
+            el => el.getAttribute('id') === som[0].getAttribute('id').replace('som_', '')
+        )
+        if (tecla.length > 0) {
+            tecla[0].classList.add('ativa');
+            tecla[0].focus();
         }
+        som[0].play();
+    }
+}
+documento.onkeyup = (evento) => {
+    teclas.forEach(tecla => {
+        tecla.classList.remove('ativa');
     })
 }
